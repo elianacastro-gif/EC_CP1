9e0db5d211ceabb84fdcdff5754fcdc8080c5b14
elianacastro-gif <eliana.castro@ucas-edu.net>
2025-08-21 09:46:13 -0600

    seasons

---
+++ notes/starters/seasons.py
@@ -0,0 +1,5 @@
+#EC 1st seasons
+
+last_name = input("what is your last name: ")
+season = input("what is your favorite season:")
+print("wow"< last_name, "son, I like"< season, "too!")
---
+++ notes/variables.py
@@ -0,0 +1,7 @@
+#EC 1st Variables Notes
+even_number = 2 
+grade = 99
+animal = "panda"
+print("grade")
+grade = 95
+print("grade")

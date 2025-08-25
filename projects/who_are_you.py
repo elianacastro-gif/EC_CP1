ea5c63b007ffdd94292d97ad1b330ff99840e2eb
elianacastro-gif <eliana.castro@ucas-edu.net>
2025-08-25 09:33:46 -0600

    who_are_you.py

---
+++ projects/who_are_you.py
@@ -0,0 +1,6 @@
+#EC, 1st Who are you
+
+name = input ("What is your name: ")
+age = input ("How old are you: ")
+color = input ("What is your favorite color: ")
+print ("My name is", name, "I am", age, "and my favorite color is", color)

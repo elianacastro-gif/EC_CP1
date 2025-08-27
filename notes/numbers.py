d10adda5e27fd6d794fd0d81f322aa06ae7c02e1
elianacastro-gif <eliana.castro@ucas-edu.net>
2025-08-27 09:54:53 -0600

    number notes

---
+++ notes/numbers.py
@@ -0,0 +1,40 @@
+#EC 1st Numbers
+
+#Integers
+age = 99999999999999
+year = 2015
+room = 200
+hour = 9
+motivation = -5
+
+#float (floating point number)
+pi = 3.14
+percent = -.75
+damage = 9.75
+
+#math
+print(5+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5
+print(5//2) #2
+print(5%2) #1
+print(5**2)  #25
+
+print(5+2 *9 + (5**5)%3)
+
+#type casting
+num = int(3.14) #does not round! gets rid of the float
+decimal = float(8)
+print (num)
+print (decimal)
+
+#using variables
+hours = 3
+minutes = 4
+
+print(hours *60+minutes)
+
+age = int(input ("how old are you: \n"))
+
+print("wow you are old that is", 525600*age, "minutes")

9969ecfa6160ce7f103c1fb28128567ba697d3fa
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-09-05 09:53:09 -0600

    idiot proof

---
+++ practices/idiot_proof.py
@@ -0,0 +1,6 @@
+#EC Idiot Proof 
+
+full_name = input("what is your name: ")
+full_name = input().strip().title()
+phone_number = input("what is your phone number: ")
+gpa = input ("what is your gpa: ")

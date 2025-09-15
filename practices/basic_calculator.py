9930c639bd1427f37777ec1dbbdc202799770055
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-09-15 09:53:15 -0600

    basic calculator

--- practices/basic_calculator.py
+++ practices/basic_calculator.py
@@ -4,7 +4,9 @@ number_1 = int(input("what is number 1: "))
 number_2 = int(input("what is number 2: "))
 
 
-
 print(number_1+number_2) 
 print(number_1-number_2) 
 print(number_1*number_2) 
+print(number_1/number_2)
+print(number_1//number_2)
+print(number_1%number_2)
--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -3,4 +3,8 @@
 full_name = input("what is your name: ")
 full_name = input().strip().title()
 phone_number = input("what is your phone number: ")
+phone_number = input().strip()
 gpa = input ("what is your gpa: ")
+gpa = input().strip()
+
+print = ("hello", full_name, "i should call", phone_number "today", "your score)

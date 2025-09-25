0e83212102153bcb4764d492394e271310107c43
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-09-25 09:53:19 -0600

    debugging

---
+++ notes/debugging.py
@@ -0,0 +1,15 @@
+#EC 1st debugging
+
+#syntax error - grammar error 
+#print "eliana"
+# =. read error message, go to that line of code, fix the syntax
+
+#logic error - we gave wrong steps
+numone = "2"
+numtwo = "5"
+
+print(numone + numtwo)
+
+# have well thought out plan, step by step go though logic
+
+#runtime error -happen when code runs 

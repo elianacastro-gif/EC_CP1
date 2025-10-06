d5b1466a207a9205076e6240872f375ec81570ef
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-10-06 09:52:13 -0600

    list manager

--- practices/list_manager.py
+++ practices/list_manager.py
@@ -1,21 +1,23 @@
 #EC 1st list manager
 
-Shopping_list = ["flour", "milk", "eggs", "flour", "cake mix", "cooking spray", "sugar"]
+Shopping_list = []
 
 while True:
-    action = int(input("\n1. add task\n2. remove task\n3. print list\n4. exit"))
+    action = input("\n1. add task\n2. remove task\n3. print list\n4. exit\n")
 
-    if action == 1:
+    if action == "1":
         task = input("enter task: ")
         Shopping_list.append(task)
-        print("f'{task}' added")
-    elif action == 4:
+        print(f"'{task}' added")
+    elif action == "4":
         break
-    elif action == 2: 
-        task = input("what item do you want to remove: ")
-        task = Shopping_list[task]
+    elif action == "2": 
+        task = int(input("what item do you want to remove: "))
+        task = Shopping_list[task-1]
         if task in Shopping_list:
             Shopping_list.remove(task)
             print(f"{task} was removed from list")
         else:
             print("item not found")
+    elif action == "3":
+        print(Shopping_list)
---
+++ practices/rock_paper_scissors.py
@@ -0,0 +1,1 @@
+#EC 1st rock paper scissors

35027a8053b8b72d79f008a69ac09bdad9ba4bbd
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-10-13 09:52:33 -0600

    password strength checker

---
+++ projects/password_strength_checker.py
@@ -0,0 +1,10 @@
+#EC 1st password strength checker 
+
+#find password
+print ("what is your password: ")
+
+#does the password have...
+list = ("length 8 characters, one uppercase, onle lowercase,one number, and a special character (@1#$%^&*)")
+
+#check password
+print ("your password is missing"(list))

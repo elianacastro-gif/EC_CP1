2da57d6b10ec66fb3ddc9f23c76b42bbe0eaaf05
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-10-14 09:51:41 -0600

    password strength checker

--- projects/password_strength_checker.py
+++ projects/password_strength_checker.py
@@ -4,7 +4,17 @@
 print ("what is your password: ")
 
 #does the password have...
-list = ("length 8 characters, one uppercase, onle lowercase,one number, and a special character (@1#$%^&*)")
-
+list = ["length 8 characters", "one uppercase", "one lowercase","one number", "a special character (@1#$%^&*)"]
 #check password
-print ("your password is missing"(list))
+print("your password is missing"(list[1-5]))
+#if your password it's not long enough etc (deemed weak) trying again/chacking password again
+no = ("exiting page")
+yes = ("checking")
+print("try again yes or no: ")
+if yes:
+    print("your password is good!")
+elif no:
+    print("exiting page.")
+#when your password is done
+
+#end of pasword

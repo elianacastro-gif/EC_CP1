67f4bc683a53c0428db88a37a8d88649ea89f0aa
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-10-27 09:51:33 -0600

    combat

---
+++ practices/combat.py
@@ -0,0 +1,17 @@
+#EC 1st 
+
+#characters and inputs stats
+
+
+
+#user combat
+
+user_attack = ()
+user_defence = ()
+
+monster_attack = ()
+monster_defence = ()
+#damage display
+#monsters attack
+#damage display
+#change HP 

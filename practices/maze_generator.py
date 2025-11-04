432d0ff0c494d6c5f1c66cd5565d8a14d0757d8f
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-11-04 09:51:41 -0700

    maze generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -2,6 +2,13 @@
 
 import turtle
 import random
+#make two lists
+grid_rows = [[1,1,0,0,1,0]]
+grid_columns = [[0,1,1,0,0,1,0,0,0,1,1,1]]
+#create systems for the maze
+#create loops
 
-size = ()
-grid = ()
+#create maze walls
+#create more codes
+#make one correct way
+#give speed and directions to the turtles

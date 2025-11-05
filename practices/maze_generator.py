5a1cb1ffedf3be146622727bb4acd9ed4efe1c12
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-11-05 09:53:14 -0700

    maze generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -3,12 +3,29 @@
 import turtle
 import random
 #make two lists
-grid_rows = [[1,1,0,0,1,0]]
-grid_columns = [[0,1,1,0,0,1,0,0,0,1,1,1]]
-#create systems for the maze
-#create loops
+grid_columns = [[random.randint(0,1)]]
+grid_columns.append(random.randint(0,1))
+grid_columns.append(random.randint(0,1))
+grid_columns.append(random.randint(0,1))
+grid_columns.append(random.randint(0,1))
+grid_columns.append(random.randint(0,1))
 
-#create maze walls
+
+grid_rows = [[random.randint(0,1)]]
+grid_rows.append(random.randint(0,1))
+grid_rows.append(random.randint(0,1))
+grid_rows.append(random.randint(0,1))
+grid_rows.append(random.randint(0,1))
+grid_rows.append(random.randint(0,1))
+#create systems for the mazem.randint(0,1)]]
+wall = turtle.Turtle
+
+#create loops
+#create maze walls randomize walls
 #create more codes
 #make one correct way
 #give speed and directions to the turtles
+#find if maze is functional
+#use turtle to draw maze
+
+turtle.done()

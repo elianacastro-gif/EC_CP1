97188f3a088c207085df3e6cf353a38c856f4900
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-11-07 09:51:32 -0700

    maze generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -3,27 +3,37 @@
 import turtle
 import random
 #make two lists
-turtle.Turtle()
-
-grid_columns = [[random.randint(0,1)]]
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+maze = turtle.Turtle()
 
 
-grid_rows = [[random.randint(0,1)]]
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+
+
+
+
+
+
+
+
+grid_columns = (random.randint(0,1))
+grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+
+
+grid_rows = (random.randint(0,1))
+grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
 
 
 maze_draw = turtle.Turtle()
 maze_draw.hideturtle()
-maze_draw.penup()
+maze_draw.penup(50,600)
+maze_draw.pendown(70,600)
 
 
 
---
+++ practices/starters/libraries.py
@@ -0,0 +1,15 @@
+#python program draw
+#spiral helix pattrn
+#using turtle program
+
+
+import turtle
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+t.speed(0)
+turtle.bgcolor('black')
+for x in range (360):
+        t.pencolor(colors[x%6])
+        t.width(x//100 + 1)
+        t.forward(x)
+        t.left(59)

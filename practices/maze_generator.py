8344d73c7b90d816578d333d2dbb6fe86a08329a
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-11-11 09:53:09 -0700

    maze generator

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -15,25 +15,30 @@ maze = turtle.Turtle()
 
 
 grid_columns = (random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_columns(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_columns=[
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))],
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))],
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,
+    1)),random.randint(0,1)]
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))]
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))]]
 
 
 grid_rows = (random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
-grid_rows(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))(random.randint(0,1))
+grid_rows =[
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))]
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))]
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randin(1))(random.randint(0,1))]
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))]
+    [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))]]
 
 
 maze_draw = turtle.Turtle()
 maze_draw.hideturtle()
 maze_draw.penup(50,600)
 maze_draw.pendown(70,600)
+
+
 
 
 
--- practices/order.py
+++ practices/order.py
@@ -1,3 +1,10 @@
 #EC order 
 #maked dictionarie of the lists
-#add 
+#need at least 1 drink, a main course,m and two side dishes
+#add amount of money to buy
+
+menu: [
+    {"Horchata": }
+
+
+]

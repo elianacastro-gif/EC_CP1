fa759c5fa8599133babb570ffa7fbe79c118e3f0
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-11-13 09:53:44 -0700

    orderup

--- practices/maze_generator.py
+++ practices/maze_generator.py
@@ -30,14 +30,20 @@ grid_rows =[
     [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))],
     [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))],
     [(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1)),(random.randint(0,1))]]
+
+
+
 for length in grid_rows:
     for wall in length:
-        if wall >= (0,1):
-            maze.penup(50)
-        if wall <= (0,1):
-            maze.pendown(50)
-
-
+        if wall >= 0:
+            maze.penup()
+            maze.forward(50)
+        if wall <= 1:
+            maze.pendown()
+            maze.forward(50)
+    maze.penup()
+    maze.teleport(0,50)
+  
 
 
 maze_draw = turtle.Turtle()
--- practices/order.py
+++ practices/order.py
@@ -3,9 +3,9 @@
 #need at least 1 drink, a main course,m and two side dishes
 #add amount of money to buy
 
-menu: [
-    {"Horchata":6.47, "(any) soda":4.16, "Agua De Jamaica":4.20} 
-    {"Carne Asada tacos":10.39, "pork tacos":9.10, ""}
-
-
+menu = [
+    {"Horchata":6.47, "soda":4.16, "Agua De Jamaica":4.20, "water":1.00},
+    {"Carne Asada tacos":10.39, "pork tacos":9.10, "fish tacos":10.50 "meat quesadilla":9.25},
+    {"rice":1.10, "extra meat":1.50, "beans":1.10, "cilantro":0.50}
 ]
+print("welcome to 'Mexican on the Road', my taco truck!")

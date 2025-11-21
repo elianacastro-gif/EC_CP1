9bcbe1ac98828d11dbeb5247f949359324caa724
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-11-21 09:51:34 -0700

    factorial

---
+++ practices/factorial.py
@@ -0,0 +1,18 @@
+#EC 1st factorial code
+
+#get a number input for a sentence that explains what a factorial is
+number = input(5*5,5*4,5*3,5*2,5*1)
+#take their inputs and check to see if it is a number that is positive
+number
+#for the  numbers multiply itself minus one each time until it is multiply by one
+#once it is one print the product and the originial number
+
+#input = 1
+#break
+#print (f"{inputs} and {original}")
+
+
+
+#take their inputs and chack if it is an intger then use .strip() 
+#else:
+#print they need to do better and try again
---
+++ practices/starters/pseudo_turtle.py
@@ -0,0 +1,11 @@
+#EC 1st turtle pseudocode starter
+import turtle
+
+#define function
+length = ()
+
+def draw_branch():
+ if length > 5:
+    for i in range(3):
+        turtle.forward()
+        draw_branch

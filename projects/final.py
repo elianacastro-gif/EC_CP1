97179c4bc7ab2c06c44db05c43e3be5a9afcb718
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-12-02 09:52:51 -0700

    final

---
+++ projects/final.py
@@ -0,0 +1,64 @@
+#EC 1st Final
+
+#add stats of each character 
+#character one BC
+
+#Character two LK
+
+#character three CB
+
+#character four HJ
+
+#character five JS
+
+#character six LX
+
+#character seven SM
+
+#character eight IN
+
+#final boss JYP 
+
+
+
+#name all the rooms and turn them into varibles
+#name all the weapons and what happens/whose in the room
+#start area (inside front company building)
+#choose which of 8 rooms to go to describe how to get there and what surroundings are like
+#make a path and ask which on they will take 
+
+
+#ask what room they want to go into 
+#make it so whiche ever room they choose it will go to that rooms design and story
+
+
+
+#named variable of room (1)
+    #what room looks is like in a print statement
+#items in the room and how you get them what the do
+#whose in the room and how do you fight them? write code to fight
+    #import random to generate numbers for the fight
+
+
+#named varible of room (2)
+#what room looks like and how it works 
+#add any items and how they are used/how to get them
+#set up fighting code
+#figure out how to do the dance fight
+
+
+#named varibale of room (3)
+#describe room in a print statement and hwat youll do
+#add any items if there are any
+#set up fighting code and how to play/win the game
+#show your statd for rapping and his stats to start the battle
+#add a random number within your range and roll to see who wins
+#add the print stament that you win when you win and how much your stats go up
+#if lose give them a choice to replay till they win
+
+
+#named value for room(4)
+
+
+
+

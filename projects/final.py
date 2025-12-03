429ff63a98118e8b5515affc6bb48eac7a4c64ea
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-12-03 09:52:18 -0700

    final

--- projects/final.py
+++ projects/final.py
@@ -38,27 +38,88 @@
 #items in the room and how you get them what the do
 #whose in the room and how do you fight them? write code to fight
     #import random to generate numbers for the fight
+#let them leave the room
 
+#describe paths and rooms they can enter
 
 #named varible of room (2)
 #what room looks like and how it works 
 #add any items and how they are used/how to get them
 #set up fighting code
 #figure out how to do the dance fight
+#let them leave the room
 
+#describe paths and rooms they can enter
 
 #named varibale of room (3)
 #describe room in a print statement and hwat youll do
 #add any items if there are any
 #set up fighting code and how to play/win the game
-#show your statd for rapping and his stats to start the battle
+#show your stats for rapping and his stats to start the battle
 #add a random number within your range and roll to see who wins
 #add the print stament that you win when you win and how much your stats go up
 #if lose give them a choice to replay till they win
+#let them leave room
 
+#describe paths and rooms they can enter
 
 #named value for room(4)
+#describe the room and what you will have to do/movements
+#add any of the items and how you can get them
+#add who is in the room and how you will fight them
+#set up fighting code
+#show stats of each of the characters
+#once they win print they have won and give them their "prize"
+#if they lost give hem ths chance to retry
+#let them leave the room
 
+#describe paths and rooms they can enter
 
+#named variable of room (5)
+#describe room and movements  also who is in the room
+#add any items how they work/how to get
+#who will you g=fight and how to win
+#fighting code 
+#if won their stats go up (one of three)
+#if lost let them retry
+#let the leave the room
 
+#describe path and rooms they can enter
 
+#named variable room (6)
+#add the description of the room
+#whose inthe room and how to defeat them
+#any items they can get if any
+#fighting code and if they win or lose
+#let them leave room
+
+#describe path and rooms they can enter
+
+#named variable room (7)
+#room description
+#add any items if 
+#whose in the room and how to defeat them
+#fighting code  if they win/lose
+#let them leave the room
+
+#descriibe which rooms they can choose (out of 8)
+
+#room (8)
+#add description
+#who is in the room and how to beat them in your fight
+#add any items
+#if they win or lose add the print statement for it and add ore to their stats(one of 3)
+#let them leave room
+
+#once you defeat evceryone you will get a story that will lead you to the concert venue 
+
+#venue/area (9)
+#add who the person is you are fighting and how to beat him.
+#you will use all the stats youve earned. His stats will show up as you battle after each attack
+#make battle code (damage and health)
+#ask what attack they want (any of 4) ask after every attack
+#add print statement that theyve won 
+#if they lose give them an option to retry
+
+#after the game is done. Ask if they want to play again
+#if yes then bring them back to the start

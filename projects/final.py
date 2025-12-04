94ae8cf421c2d7dff16c973d4c02d07c97e83134
eliana.castro@ucas-edu.net <eliana.castro@ucas-edu.net>
2025-12-04 09:54:14 -0700

    final

--- projects/final.py
+++ projects/final.py
@@ -2,6 +2,7 @@
 
 #add stats of each character 
 #character one BC
+#health, damage/attack, 
 
 #Character two LK
 
@@ -29,31 +30,30 @@
 
 
 #ask what room they want to go into 
-#make it so whiche ever room they choose it will go to that rooms design and story
+#make it so which ever room they choose it will go to that rooms design and story
 
 
 
-#named variable of room (1)
-    #what room looks is like in a print statement
-#items in the room and how you get them what the do
-#whose in the room and how do you fight them? write code to fight
-    #import random to generate numbers for the fight
+#named variable of room (1/rec studio)
+#what room looks is like in a print statement
+#items in the room and how you get them what the do (lightstick that gives health)
+#whose in the room (BC) and how do you fight them? write code to fight
+#import random to generate numbers for the fight
 #let them leave the room
 
 #describe paths and rooms they can enter
 
-#named varible of room (2)
-#what room looks like and how it works 
-#add any items and how they are used/how to get them
+#named varible of room (2/dance pract. room)
+#what room looks like and how it works. whose in the room (LK)
 #set up fighting code
 #figure out how to do the dance fight
 #let them leave the room
 
 #describe paths and rooms they can enter
 
-#named varibale of room (3)
-#describe room in a print statement and hwat youll do
-#add any items if there are any
+#named varibale of room (3/)
+#describe room in a print statement and what youll do 
+#add whose in the room (CB)
 #set up fighting code and how to play/win the game
 #show your stats for rapping and his stats to start the battle
 #add a random number within your range and roll to see who wins
@@ -63,10 +63,9 @@
 
 #describe paths and rooms they can enter
 
-#named value for room(4)
+#named value for room(4/dance prat. room 2nd)
 #describe the room and what you will have to do/movements
-#add any of the items and how you can get them
-#add who is in the room and how you will fight them
+#add who is in the room (HJ) and how you will fight them
 #set up fighting code
 #show stats of each of the characters
 #once they win print they have won and give them their "prize"
@@ -75,46 +74,44 @@
 
 #describe paths and rooms they can enter
 
-#named variable of room (5)
-#describe room and movements  also who is in the room
-#add any items how they work/how to get
+#named variable of room (5/)
+#describe room and movements  also who is in the room (JS)
 #who will you g=fight and how to win
 #fighting code 
 #if won their stats go up (one of three)
 #if lost let them retry
-#let the leave the room
+#let them leave the room
 
 #describe path and rooms they can enter
 
-#named variable room (6)
+#named variable room (6/dorm room Kit.)
 #add the description of the room
-#whose inthe room and how to defeat them
-#any items they can get if any
+#whose in the room (LX) and how to defeat them
+#any items they can get if any (frying pan used as a weapon)
 #fighting code and if they win or lose
 #let them leave room
 
 #describe path and rooms they can enter
 
-#named variable room (7)
+#named variable room (7/vocal room)
 #room description
-#add any items if 
-#whose in the room and how to defeat them
+#add any items if (microphone dropped after you beat him)
+#whose in the room (SM) and how to defeat them
 #fighting code  if they win/lose
 #let them leave the room
 
 #descriibe which rooms they can choose (out of 8)
 
-#room (8)
+#room (8/)
 #add description
-#who is in the room and how to beat them in your fight
-#add any items
+#who is in the room (IN)and how to beat them in your fight
 #if they win or lose add the print statement for it and add ore to their stats(one of 3)
 #let them leave room
 
 #once you defeat evceryone you will get a story that will lead you to the concert venue 
 
-#venue/area (9)
-#add who the person is you are fighting and how to beat him.
+#area 9 the venue
+#add who the person is you are fighting (JYP) and how to beat him.
 #you will use all the stats youve earned. His stats will show up as you battle after each attack
 #make battle code (damage and health)
 #ask what attack they want (any of 4) ask after every attack

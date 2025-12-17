11b37eb0bbf1bbffb00de2f4d351e69b16c43628
elianacastro-gif <eliana.castro@ucas-edu.net>
2025-12-16 19:25:17 -0700

    Update final.py

--- projects/final.py
+++ projects/final.py
@@ -74,15 +74,18 @@ room = (Room_1, Room_2, Room_3, Room_4,Room_5, Room_6, Room_7, Room_8)
 
 
 #named variable of room (1/rec studio)
-
 #what room looks is like in a print statement
-if room == Room_1:
-    print("you have entered the recording studio. There is only one light in the room above the table. oh! BC is here!")
-else:
-    print(Room_2-Room_8)
+def room ():
+    if room == Room_1:
+        print("you have entered the recording studio. There is only one light in the room above the table. oh! BC is here!")
+    else:
+        print("you have entered (Room_2-Room_8))
+              
 
 #items in the room and how you get them what the do (lightstick that gives health)
+game =()
 #whose in the room (BC) and how do you fight them? write code to fight
+print("You and BC will fight to see who can write the most lyrics. it will be an easy gae of ne round"
 #import random to generate numbers for the fight
 import random
 
@@ -110,38 +113,83 @@ else:
 
 print("your turn is over")
 
+while game:
+    BC_hp -= 1000
+if BC_hp <= 0:
+    print("you win")
+    game = False
 
 #let them leave the room
+leave_room=()
+def leave_room():
+    print("leaving room")
+    
 
 
 
 #describe paths and rooms they can enter
-
+Print("you are in the hallway again. Which room do you want to go into: )
 #named varible of room (2/dance pract. room)
 Room_2 =()
 #what room looks like and how it works. whose in the room (LK)
-if room == Room_2:
-    print("you have entered the dance practice room. The room is big and has a long lenth mirror on one of the walls. You look around and...oh! LK is here!")
-else: 
-    print (Room_1-Room_8)
+def room():
+    if room == Room_2:
+        print("you have entered the dance practice room. The room is big and has a long lenth mirror on one of the walls. You look around and...oh! LK is here!")
+    else: 
+        print (Room_1-Room_8)
+
+
+
 #set up fighting code
 #figure out how to do the dance fight
-#let them leave the room
-
 #describe paths and rooms they can enter
-
+print("your ack in the hallway. which room should we go into next: ")
 #named varibale of room (3/)
 Room_3 =()
 #describe room in a print statement and what youll do 
 #add whose in the room (CB)
-if room == Room_3:
-    print("you have entered CB's dorm room. You look around and see CB on the couch. He looks ready for that rap battle.")
+def room():
+    if room == Room_3:
+        print("you have entered CB's dorm room. You look around and see CB on the couch. He looks ready for that rap battle.")
 #set up fighting code and how to play/win the game
+print("you will compete in a battle with CB on who can rap the fastest)
 #show your stats for rapping and his stats to start the battle
 #add a random number within your range and roll to see who wins
 #add the print stament that you win when you win and how much your stats go up
 #if lose give them a choice to replay till they win
+hit_roll = random.randint(10-60) + CB_damage
+if hit_roll == 20:
+    print("you got a crit! that means you get to roll for damage twice!")
+    damage_roll = random.randint(10,60) + random.randint(10,80) + player_damage
+    print(f"you did {damage_roll-CB_defence} damage.")
+    CB_hp -= (damage_roll-CB_defence)
+elif  hit_roll == 1:
+    print("you roll a critical failure! now the monster gets to attack you!")
+    damage_roll = random.randint(10,60) + CB_damage
+    player_hp -= (damage_roll)
+    print(f"CB rolled {damage_roll}, you hp is now {player_hp} ")
+elif hit_roll + player_damage >= 12:
+    print("you hit! roll for damage!")
+    damage_roll = random.randint(10,60) + player_damage
+    if damage_roll > CB_defence:
+        print(f"you did {damage_roll-CB_defence} damage.")
+        CB_hp -= (damage_roll-CB_defence)
+    else:
+        print("you did no damage.")
+else:
+    print("you missed.")
+
+print("your turn is over")
+
+while game:
+    CB_hp -= 1000
+if CB_hp <= 0:
+    print("you win")
+    game = False
 #let them leave room
+print("leaving room")
+
+
 
 #describe paths and rooms they can enter
 
@@ -149,49 +197,178 @@ if room == Room_3:
 Room_4 =()
 #describe the room and what you will have to do/movements
 #add who is in the room (HJ) and how you will fight them
-if room == Room_4:
-    print("You have entered the 2nd dance practice room. The room is smaller but it still has that long length mirror on the wall.' Oop...HJ there you are!' ")
+def room():
+    if room == Room_4:
+        print("You have entered the 2nd dance practice room. The room is smaller but it still has that long length mirror on the wall.' Oop...HJ there you are!' ")
 #set up fighting code
 #show stats of each of the characters
 #once they win print they have won and give them their "prize"
 #if they lost give hem ths chance to retry
-#let them leave the room
 
+hit_roll = random.randint(10-60) + HJ_damage
+if hit_roll == 20:
+    print("you got a crit! that means you get to roll for damage twice!")
+    damage_roll = random.randint(10,60) + random.randint(10,60) + player_damage
+    print(f"you did {damage_roll-HJ_defence} damage.")
+    HJ_hp -= (damage_roll-HJ_defence)
+elif  hit_roll == 1:
+    print("you roll a critical failure! now the monster gets to attack you!")
+    damage_roll = random.randint(10,60) + HJ_damage
+    player_hp -= (damage_roll)
+    print(f"HJ rolled {damage_roll}, you hp is now {player_hp} ")
+elif hit_roll + player_damage >= 12:
+    print("you hit! roll for damage!")
+    damage_roll = random.randint(10,60) + player_damage
+    if damage_roll > HJ_defence:
+        print(f"you did {damage_roll-CB_defence} damage.")
+        HJ_hp -= (damage_roll-HJ_defence)
+    else:
+        print("you did no damage.")
+else:
+    print("you missed.")
+
+print("your turn is over")
+
+while game:
+    HJ_hp -= 1000
+if HJ_hp <= 0:
+    print("you win")
+    game = False
+#let them leave the room
+print("leaving the room")
 #describe paths and rooms they can enter
 
 #named variable of room (5/)
 Room_5 =()
 #describe room and movements  also who is in the room (JS)
-if room == Room_5:
-    print("You have entered the Cafeteria. The room is full of staff eating food. You keep walking and find JS who is eating. 'Now is not the time for eating JS!' ")
+def room():
+    if room == Room_5:
+        print("You have entered the Cafeteria. The room is full of staff eating food. You keep walking and find JS who is eating. 'Now is not the time for eating JS!' ")
 #who will you fight and how to win
 #fighting code 
 #if won their stats go up (one of three)
 #if lost let them retry
+
+hit_roll = random.randint(10-60) + JS_damage
+if hit_roll == 20:
+    print("you got a crit! that means you get to roll for damage twice!")
+    damage_roll = random.randint(10,60) + random.randint(10,60) + player_damage
+    print(f"you did {damage_roll-JS_defence} damage.")
+    JS_hp -= (damage_roll-HJ_defence)
+elif  hit_roll == 1:
+    print("you roll a critical failure! now the monster gets to attack you!")
+    damage_roll = random.randint(10,60) + JS_damage
+    player_hp -= (damage_roll)
+    print(f"JS rolled {damage_roll}, you hp is now {player_hp} ")
+elif hit_roll + player_damage >= 12:
+    print("you hit! roll for damage!")
+    damage_roll = random.randint(10,60) + player_damage
+    if damage_roll > JS_defence:
+        print(f"you did {damage_roll-JS_defence} damage.")
+        JS_hp -= (damage_roll-JS_defence)
+    else:
+        print("you did no damage.")
+else:
+    print("you missed.")
+
+print("your turn is over")
+
+while game:
+   JS_hp -= 1000
+if JS_hp <= 0:
+    print("you win")
+    game = False
 #let them leave the room
+print("leaving the room")
 
 #describe path and rooms they can enter
+print(you are back in the hallway. which room do you want to enter: )
+
+
 
 #named variable room (6/dorm room Kit.)
 Room_6 =()
 #add the description of the room
 #whose in the room (LX) and how to defeat them
-if room == Room_6:
-    print("You have entered LX's dorm room. You can smell a scent of food. You walk and find LX in the kitchen. 'LX! Why are you cooking?!' ")
+def room ():
+    if room == Room_6:
+        print("You have entered LX's dorm room. You can smell a scent of food. You walk and find LX in the kitchen. 'LX! Why are you cooking?!' ")
 #any items they can get if any (frying pan used as a weapon)
 #fighting code and if they win or lose
-#let them leave room
+
+
+hit_roll = random.randint(10-60) + LX_damage
+if hit_roll == 20:
+    print("you got a crit! that means you get to roll for damage twice!")
+    damage_roll = random.randint(10,60) + random.randint(10,60) + player_damage
+    print(f"you did {damage_roll-HJ_defence} damage.")
+    LX_hp -= (damage_roll-LX_defence)
+elif  hit_roll == 1:
+    print("you roll a critical failure! now the monster gets to attack you!")
+    damage_roll = random.randint(10,60) + LX_damage
+    player_hp -= (damage_roll)
+    print(f"LX rolled {damage_roll}, you hp is now {player_hp} ")
+elif hit_roll + player_damage >= 12:
+    print("you hit! roll for damage!")
+    damage_roll = random.randint(10,60) + player_damage
+    if damage_roll > LX_defence:
+        print(f"you did {damage_roll-LX_defence} damage.")
+        LX_hp -= (damage_roll-LX_defence)
+    else:
+        print("you did no damage.")
+else:
+    print("you missed.")
+
+print("your turn is over")
+
+while game:
+    LX_hp -= 1000
+if LX_hp <= 0:
+    print("you win")
+    game = False
+#let them leave the room
+print("leaving the room")
 
 #describe path and rooms they can enter
 
 #named variable room (7/vocal room)
 Room_7 =()
 #room description
-if room == Room_7:
-    print("You have entered the vocal room. 'hmm I wonder if SM is in here...'")
+def room():
+    if room == Room_7:
+        print("You have entered the vocal room. 'hmm I wonder if SM is in here...'")
 #add any items if (microphone dropped after you beat him)
 #whose in the room (SM) and how to defeat them
 #fighting code  if they win/lose
+hit_roll = random.randint(10-60) + SM_damage
+if hit_roll == 20:
+    print("you got a crit! that means you get to roll for damage twice!")
+    damage_roll = random.randint(10,60) + random.randint(10,60) + player_damage
+    print(f"you did {damage_roll-SM_defence} damage.")
+    SM_hp -= (damage_roll-SM_defence)
+elif  hit_roll == 1:
+    print("you roll a critical failure! now the monster gets to attack you!")
+    damage_roll = random.randint(10,60) + SM_damage
+    player_hp -= (damage_roll)
+    print(f"SM rolled {damage_roll}, you hp is now {player_hp} ")
+elif hit_roll + player_damage >= 12:
+    print("you hit! roll for damage!")
+    damage_roll = random.randint(10,60) + player_damage
+    if damage_roll > SM_defence:
+        print(f"you did {damage_roll-SM_defence} damage.")
+        SM_hp -= (damage_roll-SM_defence)
+    else:
+        print("you did no damage.")
+else:
+    print("you missed.")
+
+print("your turn is over")
+
+while game:
+    SM_hp -= 1000
+if SM_hp <= 0:
+    print("you win")
+    game = False
 #let them leave the room
 
 #descriibe which rooms they can choose (out of 8)
@@ -200,9 +377,43 @@ if room == Room_7:
 Room_8 = ()
 #add description
 #who is in the room (IN)and how to beat them in your fight
-if room == Room_8:
-    print("You have entered the ___. You walk and see IN.")
+def room():
+    if room == Room_8:
+        print("You have entered the ___. You walk and see IN.")
 #if they win or lose add the print statement for it and add ore to their stats(one of 3)
+
+hit_roll = random.randint(10-60) + IN_damage
+if hit_roll == 20:
+    print("you got a crit! that means you get to roll for damage twice!")
+    damage_roll = random.randint(10,60) + random.randint(10,60) + player_damage
+    print(f"you did {damage_roll-IN_defence} damage.")
+    IN_hp -= (damage_roll-HJ_defence)
+elif  hit_roll == 1:
+    print("you roll a critical failure! now the monster gets to attack you!")
+    damage_roll = random.randint(10,60) + IN_damage
+    player_hp -= (damage_roll)
+    print(f"IN rolled {damage_roll}, you hp is now {player_hp} ")
+elif hit_roll + player_damage >= 12:
+    print("you hit! roll for damage!")
+    damage_roll = random.randint(10,60) + player_damage
+    if damage_roll > IN_defence:
+        print(f"you did {damage_roll-IN_defence} damage.")
+        IN_hp -= (damage_roll-IN_defence)
+    else:
+        print("you did no damage.")
+else:
+    print("you missed.")
+
+print("your turn is over")
+
+while game:
+    IN_hp -= 1000
+if IN_hp <= 0:
+    print("you win")
+    game = False
+#let them leave the room
+print("leaving the room")
+#describe paths and rooms they can enter
 #let them leave room
 
 #once you defeat evceryone you will get a story that will lead you to the concert venue 
